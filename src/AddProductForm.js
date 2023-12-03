f9d2f157b93e2685b7061e63c107a296f62a2ddc
huaizhic <huaizhihuaizhi@gmail.com>
2023-12-03 15:56:38 +0800

    Add new CatalogCell with AddProductForm, with Supabase updating capability
    catalog list seems to update in an unusual sorting order after refresh, will look into it

--- src/AddProductForm.js
+++ src/AddProductForm.js
@@ -17,36 +17,53 @@ function AddProductForm({
   setProductCategory,
 }) {
   async function handleSubmit(e) {
-    e.preventDefault(); // prevent broswer reload
-    const newProduct = { productTitle, productPrice, productCategory };
-    setCatalog((catalog) => [newProduct, ...catalog]);
+    // prevent broswer reload
+    e.preventDefault();
+    // upload newly submitted product to supabase, and retrieve it at the same time, naming it newProduct
+    const { data: newProduct, error } = await supabase
+      .from("CatalogList")
+      .insert([{ productTitle, productPrice, productCategory }])
+      .select();
+    // console.log(newProduct);
+    // reflect changes on local UI
+    setCatalog((catalog) => [newProduct[0], ...catalog]);
+    // reset form upon submission
+    setProductTitle("");
+    setProductPrice("");
+    setProductCategory("");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Product Name"
-        value={productTitle}
-        onChange={(e) => setProductTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Price"
-        value={productPrice}
-        onChange={(e) => setProductPrice(e.target.value)}
-      />
-      <select
-        value={productCategory}
-        onChange={(e) => setProductCategory(e.target.value)}
-      >
-        <option value="">Choose category</option>
-        {FakeCategories.map((category) => (
-          <option value={category.name}>{category.name}</option>
-        ))}
-      </select>
-      <button>Submit</button>
-    </form>
+    <>
+      <div className="addProduct">
+        <h2>Add new product</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Product Name"
+            value={productTitle}
+            // whenever the user updates the input box, onChange will be triggered, setting the state of productTitle
+            onChange={(e) => setProductTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Price"
+            value={productPrice}
+            onChange={(e) => setProductPrice(e.target.value)}
+          />
+          <select
+            value={productCategory}
+            onChange={(e) => setProductCategory(e.target.value)}
+          >
+            <option value="">Choose category</option>
+            {FakeCategories.map((category) => (
+              <option value={category.name}>{category.name}</option>
+            ))}
+          </select>
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
