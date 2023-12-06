f8933c82d8c93e310a7c9d205179cbc9da183d6d
huaizhic <huaizhihuaizhi@gmail.com>
2023-12-06 18:03:29 +0800

    EditProductForm near full functionality with input validation and warning message
    No error handling yet

--- src/EditProductForm.js
+++ src/EditProductForm.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import supabase from "./supabase";
 
 const FakeCategories = [
@@ -7,10 +8,83 @@ const FakeCategories = [
 ];
 
 function EditProductForm({ setCatalog, catalog, productEdit, setProductEdit }) {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputPrice, setInputPrice] = useState("");
+  const [inputCategory, setInputCategory] = useState("");
+  const [updatedFlag, setUpdatedFlag] = useState(false);
+  const [dropdownValue, setDropdownValue] = useState("Choose product");
+
+  useEffect(
+    function () {
+      async function updateCatalog() {
+        // console.log(productEdit.productTitle);
+        if (
+          productEdit.productTitle !== null &&
+          productEdit.productPrice !== null &&
+          productEdit.productCategory !== null
+        ) {
+          const { data: updatedProduct, error } = await supabase
+            .from("CatalogList")
+            .upsert({
+              // if id exists, update data; if not insert a new row with these details
+              id: productEdit.id,
+              productTitle: productEdit.productTitle,
+              productPrice: productEdit.productPrice,
+              productCategory: productEdit.productCategory,
+            })
+            .select();
+
+          // reflect supabase changes on local UI
+          setCatalog(
+            catalog.map((product) =>
+              product.id === productEdit.id ? updatedProduct[0] : product
+            )
+          );
+
+          handleClick("Choose product");
+          setDropdownValue("Choose product");
+          // console.log(updatedProduct);
+        }
+      }
+      updateCatalog();
+    },
+    [updatedFlag]
+  );
+
+  // have to click button twice for it to work
   async function handleSubmit(e) {
     // prevent broswer reload
     e.preventDefault();
-    alert("This button is not ready yet!");
+    // alert("This button is not ready yet!");
+    if (dropdownValue === "Choose product") {
+      alert("You have not chosen a product to edit yet!");
+    } else {
+      if (
+        inputTitle === "" ||
+        Number.isNaN(inputPrice) ||
+        inputCategory === ""
+      ) {
+        alert("You have not filled in all the blanks!");
+      } else {
+        const response = window.confirm(
+          "Confirm changes. This action is irreversible"
+        ); // boolean
+        if (response === false) {
+        } else {
+          setProductEdit({
+            ...productEdit,
+            productTitle: inputTitle,
+            productPrice: inputPrice,
+            productCategory: inputCategory,
+          });
+
+          setUpdatedFlag(!updatedFlag);
+          // console.log(productEdit.productTitle);
+          // console.log(productEdit.id);
+          alert("Changes made successfully!");
+        }
+      }
+    }
   }
 
   async function handleClick(productName) {
@@ -23,64 +97,42 @@ function EditProductForm({ setCatalog, catalog, productEdit, setProductEdit }) {
         productPrice: null,
         productCategory: null,
       });
+      setInputTitle("");
+      setInputPrice("");
+      setInputCategory("");
+      setDropdownValue("Choose product");
     } else {
+      setDropdownValue(productName);
       const arrayOfObject = catalog.filter(
         (product) => product.productTitle === productName
       );
       setProductEdit(arrayOfObject[0]); // arrayOfObject[0] is an object type (of the specified product)
+      setInputTitle(arrayOfObject[0].productTitle);
+      setInputPrice(arrayOfObject[0].productPrice);
+      setInputCategory(arrayOfObject[0].productCategory);
+      // console.log(arrayOfObject[0].productTitle);
     }
   }
 
-  function EditingPanel() {
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="New Product Name"
-          value={productEdit.productTitle}
-          onChange={
-            (e) =>
-              setProductEdit({ ...productEdit, productTitle: e.target.value }) // editing attribute in object
-          }
-        />
-        <input
-          type="number"
-          placeholder="New Price"
-          value={productEdit.productPrice}
-          onChange={(e) =>
-            setProductEdit({
-              ...productEdit,
-              productPrice: e.target.valueAsNumber,
-            })
-          }
-        />
-        <select
-          value={productEdit.productCategory}
-          onChange={(e) =>
-            setProductEdit({ ...productEdit, productCategory: e.target.value })
-          }
-        >
-          <option value="">Choose new category</option>
-          {FakeCategories.map((category) => (
-            <option value={category.name}>{category.name}</option>
-          ))}
-        </select>
-        <button>Update</button>
-      </form>
-    );
-  }
+  // function EditingPanel() {
+  //   return (
+
+  //   );
+  // }
 
   return (
     <>
       <div className="editProduct">
-        <h2>Edit existing product(s) (not ready yet)</h2>
+        <h2>Edit existing product(s)</h2>
         <p>
-          {" "}
           Idea: When a product is selected in the dropdown, current products
           details should be pre-filled in the inputs for user to update
         </p>
         <form onSubmit={handleSubmit}>
-          <select onChange={(e) => handleClick(e.target.value)}>
+          <select
+            value={dropdownValue}
+            onChange={(e) => handleClick(e.target.value)}
+          >
             <option value="Choose product">Choose product</option>
             {catalog.map((product) => (
               <option value={product.productTitle}>
@@ -88,8 +140,30 @@ function EditProductForm({ setCatalog, catalog, productEdit, setProductEdit }) {
               </option>
             ))}
           </select>
+
+          <input
+            type="text"
+            placeholder="New Product Name"
+            value={inputTitle}
+            onChange={(e) => setInputTitle(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="New Price"
+            value={inputPrice}
+            onChange={(e) => setInputPrice(e.target.valueAsNumber)}
+          />
+          <select
+            value={inputCategory}
+            onChange={(e) => setInputCategory(e.target.value)}
+          >
+            <option value="">Choose new category</option>
+            {FakeCategories.map((category) => (
+              <option value={category.name}>{category.name}</option>
+            ))}
+          </select>
+          <button>Update</button>
         </form>
-        <EditingPanel />
       </div>
     </>
   );
